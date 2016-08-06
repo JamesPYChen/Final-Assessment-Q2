a132f2d8b3b094ab13705bac6876a0b4e7833afd
James PY Chen <jamespoyu@gmail.com>
2016-08-06 15:43:08 +0800

    complete

---
+++ finalAssessmentQ2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  finalAssessmentQ2
+//
+//  Created by jamespoyu on 2016/8/6.
+//  Copyright © 2016年 AlphaCamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
